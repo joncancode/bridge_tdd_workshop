ea104316452854dd63252186478ff0cec652b4a6
joncancode <jc.haines19@gmail.com>
2017-11-11 11:41:23 -0500

    first shallow tests added, inc the intro of a spy

--- client/components/text_input/index.jsx
+++ client/components/text_input/index.jsx
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 
 export default class TextInput extends Component {
-  render() {}
+
+  handleClick() {
+    // e.preventDefault();
+    console.log('clicked');
+    }
+
+  render() {
+    return (
+      <div className="unique">
+        <h2>this is the test</h2>
+        <button onClick={(e)=>this.handleClick(e)}>Click</button>
+    </div>
+    )
+  }
 }
--- client/components/text_input/index_test.js
+++ client/components/text_input/index_test.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+import sinon from 'sinon';
 
 import TextInput from './index';
 
 describe('<TextInput />', () => {
+  it('renders text when text is added', () => {
+    const wrapper = render(<TextInput />);
+    expect(wrapper.text()).toContain('this is the test');
+  });
 
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = shallow(<TextInput onButtonClick={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
+  });
+
+
+});
+
+describe('A spy', function() {
+  var foo,
+    bar = null;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      }
+    };
+
+    spyOn(foo, 'setBar');
+
+    foo.setBar(123);
+    foo.setBar(456, 'another param');
+  });
+
+  it('tracks that the spy was called', function() {
+    expect(foo.setBar).toHaveBeenCalled();
+  });
 });
